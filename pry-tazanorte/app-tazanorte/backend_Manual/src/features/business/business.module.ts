cea220ac7b183069fccb60171738adc800e37931
deisireed <deisireedoviedo08@gmail.com>
2026-09-16 23:09:05 -0500

    10.25

--- pry-tazanorte/app-tazanorte/backend_Manual/src/features/business/business.module.ts
+++ pry-tazanorte/app-tazanorte/backend_Manual/src/features/business/business.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
 import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [ClientsModule, ProductsModule, OrdersModule],
-  exports: [ClientsModule, ProductsModule, OrdersModule],
+  imports: [ClientsModule, ProductsModule, SalesModule],
+  exports: [ClientsModule, ProductsModule, SalesModule],
 })
 export class BusinessModule {}
